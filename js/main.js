ec21f666e5e75966472a4a4e7ec93c60650710e2
JoEric Mercado <joeric.m@scale.tech>
2019-02-18 18:04:03 -0800

    Making changes to how it parses the URL string.

--- js/main.js
+++ js/main.js
@@ -1,164 +1,182 @@
-let variable = {
-    inputForm: document.getElementById("input"),
-    outputContainer: document.getElementsByClassName("outputContainer"),
-    tldContainer: document.getElementById("tld"),
-    relativePathContainer: document.getElementById("relativePath"),
-    messageContainer: document.getElementById("message"),
-    url: "",
-    tld: "",
-    relativePath: "",
-    isHide: true,
+(() => {
+    console.log(3);
+    console.log(4);
+    inputForm = document.getElementById("input");
+    outputContainer = document.getElementsByClassName("outputContainer");
+    tldContainer = document.getElementById("tld");
+    relativePathContainer = document.getElementById("relativePath");
+    messageContainer = document.getElementById("message");
+    url = "";
+    tld = "";
+    relativePath = "";
+    isHide = true;
 
-    https: "https://",
-    http: "http://",
-    tempURL: "",
-    tempDomain: "",
-    url: [],
-    isHTTPS: false
-}
+    https = "https://";
+    http = "http://";
+    tempURL = "";
+    tempDomain = "";
+    isHTTPS = false;
+    url = [];
+    //0 = Protocol
+    //1 = Subdomain
+    //2 = Domain
+    //3 = Domain Extension
+    //4 = Parameters
 
-let listener = {
-    setInput: () => {
-        variable.inputForm.addEventListener("input", parseURL.control);
-    },
-    setTLD: () => {
-        variable.tldContainer.addEventListener("click", parseURL.copy);
-    },
-    setRelativePath: () => {
-        variable.relativePathContainer.addEventListener("click", parseURL.copy);
-    },
-    removeInput: () => {
-        variable.inputForm.removeEventListener("input", parseURL.parse);
-    },
-    removeTLD: () => {
-        variable.tldContainer.removeEventListener("click", parseURL.copy);
-    },
-    removeRelativePath: () => {
-        variable.relativePathContainer.removeEventListener("click", parseURL.copy);
-    }
-}
-
-let message = {
-    setCopied: () => {
-        variable.messageContainer.innerHTML = "Copied!";
-        setTimeout(message.blank(), 2000);
-    },
-    blank: () => {
-        variable.messageContainer.innerHTML = "";
-    }
-}
-let string = {
-    substr: val => parseURL.getURL().substr(0, val).toLowerCase()
-}
-let parseURL = {
-    main: () => {
-        listener.setInput();
-    },
-    setProtocol: () => {
-        if(string.substr(8)===variable.https) {
-            console.log("has HTTPS");
-            variable.isHTTPS = true;
-            return variable.https;
-        } else if(string.substr(7)==="http://") {
-            console.log("has HTTP");
-            //Add S
-            variable.url[0] = "http://";
-            variable.isHTTPS = false;
-            //Proceed to check TLD
-        } else {
-            console.log("Does not have ANY protocol.");
-            //Add https
-            //Save HTTPS to urlArr[0]
-            //Proceed to check TLD
+    let listener = {
+        setInput: () => {
+            inputForm.addEventListener("input", parseURL.control);
+        },
+        setTLD: () => {
+            tldContainer.addEventListener("click", parseURL.copy);
+        },
+        setRelativePath: () => {
+            relativePathContainer.addEventListener("click", parseURL.copy);
+        },
+        removeInput: () => {
+            inputForm.removeEventListener("input", parseURL.parse);
+        },
+        removeTLD: () => {
+            tldContainer.removeEventListener("click", parseURL.copy);
+        },
+        removeRelativePath: () => {
+            relativePathContainer.removeEventListener("click", parseURL.copy);
         }
-    },
-    setDomain: () => {
-        return variable.tempDomain.split("/")[0];
-    },
-    setRelativePath: () => {
-        return variable.tempDomain.match(/\/.*$/i)[0];
-    },
-    output: () => {
-        console.log(variable.url[0]);
-        console.log(variable.url[1]);
-        console.log(variable.url[2]);
-    },
-    control: () => {
-        variable.tempURL = parseURL.getInput();
-        variable.url[0] = parseURL.setProtocol();
+    }
 
-        if(variable.isHTTPS) {
-            variable.tempDomain = variable.tempURL.split(variable.https)[1];
-        } else {
-            variable.tempDomain = variable.tempURL.split(variable.http)[1];
+    let message = {
+        setCopied: () => {
+            messageContainer.innerHTML = "Copied!";
+            setTimeout(message.blank(), 2000);
+        },
+        blank: () => {
+            messageContainer.innerHTML = "";
         }
+    }
+    let string = {
+        substr: val => parseURL.getURL().substr(0, val).toLowerCase()
+    }
+    let parseURL = {
+        main: () => {
+            listener.setInput();
+        },
+        setProtocol: () => {
+            if(string.substr(8)===https) {
+                console.log("has HTTPS");
+                isHTTPS = true;
+                return https;
+            } else if(string.substr(7)==="http://") {
+                console.log("has HTTP");
+                isHTTPS = false;
+                return http;
+            } else {
+                console.log("Does not have ANY protocol.");
+                isHTTPS = true;
+                return https;
+            }
+        },
+        setDomain: () => {
+            return tempDomain.split("/")[0];
+        },
+        setRelativePath: () => {
+            return tempDomain.match(/\/.*$/i)[0];
+        },
+        output: () => {
+            console.log(url[0]);
+            console.log(url[1]);
+            console.log(url[2]);
+        },
+        getProtocol: () => {
+            return parseURL.setProtocol();
+        },
+        getSubdomain: () => {
+            return parseURL.setProtocol();
+        },
+        control: () => {
+            tempURL = parseURL.getInput();
 
-        variable.url[1] = parseURL.setDomain();
-        variable.url[2] = parseURL.setRelativePath();
-        parseURL.output();
-    },
-    checkTLD: () => {
+            url[0] = parseURL.getProtocol();
+            url[1] = parseURL.getSubdomain();
+            url[2] = parseURL.getDomain();
+            url[3] = parseURL.getDomainExtension();
+            url[4] = parseURL.getParameters();
 
-    },
-    parse: () => {
-        let tempURL = parseURL.getURL();
+            if(isHTTPS) {
+                tempDomain = tempURL.split(https)[1];
+            } else {
+                tempDomain = tempURL.split(http)[1];
+            }
 
-        if(parseURL.isBlank(tempURL)!==true) {
-            if(tempURL.includes(".org")) {
-                variable.url = tempURL;
-                variable.tld = parseURL.getTLD();
-                variable.relativePath = parseURL.getRelativePath();
-                parseURL.hideOutput(false);
-                parseURL.setOutput(variable.tld, variable.relativePath);
-                listener.setTLD();
-                listener.setRelativePath();
+            url[1] = parseURL.setDomain();
+            url[2] = parseURL.setRelativePath();
+            parseURL.output();
+        },
+        checkTLD: () => {
+
+        },
+        parse: () => {
+            let tempURL = parseURL.getURL();
+
+            if(parseURL.isBlank(tempURL)!==true) {
+                if(tempURL.includes(".org")) {
+                    url = tempURL;
+                    tld = parseURL.getTLD();
+                    relativePath = parseURL.getRelativePath();
+                    parseURL.hideOutput(false);
+                    parseURL.setOutput(tld, relativePath);
+                    listener.setTLD();
+                    listener.setRelativePath();
+                } else {
+                    parseURL.setOutputBlank();
+                    listener.removeTLD();
+                    listener.removeRelativePath();
+                    parseURL.hideOutput(true);
+                }
             } else {
                 parseURL.setOutputBlank();
                 listener.removeTLD();
                 listener.removeRelativePath();
                 parseURL.hideOutput(true);
             }
-        } else {
-            parseURL.setOutputBlank();
-            listener.removeTLD();
-            listener.removeRelativePath();
-            parseURL.hideOutput(true);
-        }
-    },
-    isBlank: (e) => {
-        return x = e==="" || e===" " || e==="undefined" ? true : false;
-    },
-    hideOutput: (bool) => {
-        variable.isHide = bool;
+        },
+        isBlank: (e) => {
+            return x = e==="" || e===" " || e==="undefined" ? true : false;
+        },
+        hideOutput: (bool) => {
+            isHide = bool;
 
-        if(variable.isHide===true) {
-            variable.outputContainer[0].classList.add("hide");
-        } else {
-            variable.outputContainer[0].classList.remove("hide");
+            if(isHide===true) {
+                outputContainer[0].classList.add("hide");
+            } else {
+                outputContainer[0].classList.remove("hide");
+            }
+        },
+        getInput: () => inputForm.value,
+        getURL: () => tempURL,
+        getTLD: () => {
+            let beforeORG = url.split(".org")[0];
+            return x = beforeORG + ".org";
+        },
+        getRelativePath1: () => {
+            let afterORG = url.split(".org")[1];
+            return x = afterORG!=="" || afterORG!== "/ " ? afterORG : "";
+        },
+        copy: (e) => {
+            console.log(e.target.innerHTML);
+            message.setCopied();
+        },
+        setOutputBlank: () => {
+            tldContainer.innerHTML = "";
+            relativePathContainer.innerHTML = "";
+        },
+        setOutput: (tld, relativePath) => {
+            tldContainer.innerHTML = tld;
+            relativePathContainer.innerHTML = relativePath;
         }
-    },
-    getInput: () => variable.inputForm.value,
-    getURL: () => variable.tempURL,
-    getTLD: () => {
-        let beforeORG = variable.url.split(".org")[0];
-        return x = beforeORG + ".org";
-    },
-    getRelativePath1: () => {
-        let afterORG = variable.url.split(".org")[1];
-        return x = afterORG!=="" || afterORG!== "/ " ? afterORG : "";
-    },
-    copy: (e) => {
-        console.log(e.target.innerHTML);
-        message.setCopied();
-    },
-    setOutputBlank: () => {
-        variable.tldContainer.innerHTML = "";
-        variable.relativePathContainer.innerHTML = "";
-    },
-    setOutput: (tld, relativePath) => {
-        variable.tldContainer.innerHTML = tld;
-        variable.relativePathContainer.innerHTML = relativePath;
-    }
-}
-
-parseURL.main();
+    };
+    $(document).ready(() => {
+        console.log(1);
+        console.log(2);
+        parseURL.main();
+    });
+})();
